4be8e81fc6a33bbf1bc21782e6bc945dd9ddae9e
Dylan Doucet <ddoucet@local.sequiter.com>
2024-02-28 08:05:38 -0700

    maintatin

--- init.lua
+++ init.lua
@@ -35,6 +35,8 @@ require("lazy").setup({
 
   -- Detect tabstop and shiftwidth automatically
   "tpope/vim-sleuth",
+  -- TS errors
+  "dmmulroy/ts-error-translator.nvim",
 
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
