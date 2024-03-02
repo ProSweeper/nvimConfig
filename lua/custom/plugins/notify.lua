63f758835a0cc9c300640bc967f2510218983f6a
Dylan Doucet <ddoucet@local.sequiter.com>
2024-03-02 10:03:22 -0700

    config

--- lua/custom/plugins/notify.lua
+++ lua/custom/plugins/notify.lua
@@ -1,27 +1,28 @@
 return {
-	"rcarriga/nvim-notify",
-	config = function()
-		require("notify").setup {
-
-			background_colour = "#000000",
-			fps = 30,
-			icons = {
-				DEBUG = "",
-				ERROR = "",
-				INFO = "",
-				TRACE = "✎",
-				WARN = "",
-			},
-			level = 2,
-			minimum_width = 50,
-			render = "compact",
-			stages = "fade_in_slide_out",
-			time_formats = {
-				notification = "%T",
-				notification_history = "%FT%T",
-			},
-			timeout = 5000,
-			top_down = true,
-		}
-	end,
+	-- "rcarriga/nvim-notify",
+	-- config = function()
+	-- 	require("notify").setup {
+	--
+	-- 		background_colour = "#000000",
+	-- 		fps = 30,
+	-- 		icons = {
+	-- 			DEBUG = "",
+	-- 			ERROR = "",
+	-- 			INFO = "",
+	-- 			TRACE = "✎",
+	-- 			WARN = "",
+	-- 		},
+	-- 		level = 2,
+	-- 		minimum_width = 20,
+	-- 		maximum_width = 60,
+	-- 		render = "compact",
+	-- 		stages = "fade_in_slide_out",
+	-- 		time_formats = {
+	-- 			notification = "%T",
+	-- 			notification_history = "%FT%T",
+	-- 		},
+	-- 		timeout = 5000,
+	-- 		top_down = true,
+	-- 	}
+	-- end,
 }
