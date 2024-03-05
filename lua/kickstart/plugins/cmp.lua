59d0d932a38ab3fe8429f51a1af244ab7aef7cee
Dylan Doucet <ddoucet@local.sequiter.com>
2024-03-05 15:37:58 -0700

    snippets wooo

--- lua/kickstart/plugins/cmp.lua
+++ lua/kickstart/plugins/cmp.lua
@@ -34,6 +34,62 @@ return {
       -- See `:help cmp`
       local cmp = require 'cmp'
       local luasnip = require 'luasnip'
+      local kind_icons = {
+        Text = '',
+        Method = '󰆧',
+        Function = '󰊕',
+        Constructor = '',
+        Field = '󰇽',
+        Variable = '󰂡',
+        Class = '󰠱',
+        Interface = '',
+        Module = '',
+        Property = '󰜢',
+        Unit = '',
+        Value = '󰎠',
+        Enum = '',
+        Keyword = '󰌋',
+        Snippet = '',
+        Color = '󰏘',
+        File = '󰈙',
+        Reference = '',
+        Folder = '󰉋',
+        EnumMember = '',
+        Constant = '󰏿',
+        Struct = '',
+        Event = '',
+        Operator = '󰆕',
+        TypeParameter = '󰅲',
+      }
+
+      -- can switch the above out for these if I want
+      -- local cmp_kinds = {
+      --   Text = '  ',
+      --   Method = '  ',
+      --   Function = '  ',
+      --   Constructor = '  ',
+      --   Field = '  ',
+      --   Variable = '  ',
+      --   Class = '  ',
+      --   Interface = '  ',
+      --   Module = '  ',
+      --   Property = '  ',
+      --   Unit = '  ',
+      --   Value = '  ',
+      --   Enum = '  ',
+      --   Keyword = '  ',
+      --   Snippet = '  ',
+      --   Color = '  ',
+      --   File = '  ',
+      --   Reference = '  ',
+      --   Folder = '  ',
+      --   EnumMember = '  ',
+      --   Constant = '  ',
+      --   Struct = '  ',
+      --   Event = '  ',
+      --   Operator = '  ',
+      --   TypeParameter = '  ',
+      -- }
       require('luasnip.loaders.from_vscode').lazy_load()
       luasnip.config.setup {}
 
@@ -41,6 +97,21 @@ return {
         snippet = {
           expand = function(args)
             luasnip.lsp_expand(args.body)
+          end,
+        },
+        formatting = {
+          format = function(entry, vim_item)
+            -- Kind icons
+            vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatenates the icons with the name of the item kind
+            -- Source
+            vim_item.menu = ({
+              buffer = '[Buffer]',
+              nvim_lsp = '[LSP]',
+              luasnip = '[LuaSnip]',
+              nvim_lua = '[Lua]',
+              latex_symbols = '[LaTeX]',
+            })[entry.source.name]
+            return vim_item
           end,
         },
         completion = { completeopt = 'menu,menuone,noinsert' },
@@ -91,6 +162,22 @@ return {
           { name = 'path' },
         },
       }
+      -- gray
+      vim.api.nvim_set_hl(0, 'CmpItemAbbrDeprecated', { bg = 'NONE', strikethrough = true, fg = '#808080' })
+      -- blue
+      vim.api.nvim_set_hl(0, 'CmpItemAbbrMatch', { bg = 'NONE', fg = '#569CD6' })
+      vim.api.nvim_set_hl(0, 'CmpItemAbbrMatchFuzzy', { link = 'CmpIntemAbbrMatch' })
+      -- light blue
+      vim.api.nvim_set_hl(0, 'CmpItemKindVariable', { bg = 'NONE', fg = '#9CDCFE' })
+      vim.api.nvim_set_hl(0, 'CmpItemKindInterface', { link = 'CmpItemKindVariable' })
+      vim.api.nvim_set_hl(0, 'CmpItemKindText', { link = 'CmpItemKindVariable' })
+      -- pink
+      vim.api.nvim_set_hl(0, 'CmpItemKindFunction', { bg = 'NONE', fg = '#C586C0' })
+      vim.api.nvim_set_hl(0, 'CmpItemKindMethod', { link = 'CmpItemKindFunction' })
+      -- front
+      vim.api.nvim_set_hl(0, 'CmpItemKindKeyword', { bg = 'NONE', fg = '#D4D4D4' })
+      vim.api.nvim_set_hl(0, 'CmpItemKindProperty', { link = 'CmpItemKindKeyword' })
+      vim.api.nvim_set_hl(0, 'CmpItemKindUnit', { link = 'CmpItemKindKeyword' })
     end,
   },
 }
