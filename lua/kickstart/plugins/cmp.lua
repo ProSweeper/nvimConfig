bd5366d4acda4e4419d91ca6fad74415f5e09f27
Dylan Doucet <ddoucet@local.sequiter.com>
2024-03-05 11:28:11 -0700

    more snippet work

--- lua/kickstart/plugins/cmp.lua
+++ lua/kickstart/plugins/cmp.lua
@@ -34,6 +34,7 @@ return {
       -- See `:help cmp`
       local cmp = require 'cmp'
       local luasnip = require 'luasnip'
+      require('luasnip.loaders.from_vscode').lazy_load()
       luasnip.config.setup {}
 
       cmp.setup {
