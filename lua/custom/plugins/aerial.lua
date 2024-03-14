bc3ac1ca5f8e6241a175606ae2118c485115e1bd
Dylan Doucet <ddoucet@local.sequiter.com>
2024-03-14 13:20:46 -0600

    fix plugins

--- lua/custom/plugins/aerial.lua
+++ lua/custom/plugins/aerial.lua
@@ -1,13 +1,5 @@
 return {
   'stevearc/aerial.nvim',
-  event = 'VeryLazy',
-  cmd = {
-    'AerialToggle',
-    'AerialOpen',
-    'AerialNavOpen',
-    'AerialInfo',
-    'AerialClose',
-  },
   opts = {
     filter_kind = { -- Symbols that will appear on the tree
       -- "Class",
@@ -25,8 +17,6 @@ return {
     link_tree_to_folds = false,
     attach_mode = 'global',
     backends = { 'lsp', 'treesitter', 'markdown', 'man' },
-    disable_max_lines = vim.g.big_file.lines,
-    disable_max_size = vim.g.big_file.size,
     layout = { min_width = 28 },
     show_guides = true,
     guides = {
@@ -46,19 +36,9 @@ return {
       [']]'] = false,
     },
   },
-  config = function(_, opts)
-    require('aerial').setup(opts)
-    -- HACK: The first time you opened aerial on a session, close all folds.
-    vim.api.nvim_create_autocmd('FileType', {
-      desc = 'Aerial: The first time its open on a session, close all folds.',
-      callback = function()
-        local is_aerial = vim.bo.filetype == 'aerial'
-        local is_ufo_available = require('base.utils').is_available 'nvim-ufo'
-        if is_ufo_available and is_aerial and vim.g.new_aerial_session == nil then
-          vim.g.new_aerial_session = false
-          require('aerial').tree_set_collapse_level(0, 0)
-        end
-      end,
-    })
-  end,
+  -- Optional dependencies
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
+    'nvim-tree/nvim-web-devicons',
+  },
 }
