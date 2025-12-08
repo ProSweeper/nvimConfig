f5d4bbe00623df64cec8c723f4cfbcc9bcb17bdc
ddoucet <ddoucet@local.sequiter.com>
2025-12-08 11:05:26 -0700

    make hidden and git ignored files show up in search

--- lua/plugins/extend-snacks.lua
+++ lua/plugins/extend-snacks.lua
@@ -8,6 +8,18 @@ return {
       sources = {
         explorer = {
           layout = { layout = { position = "right" } },
+          hidden = true,
+          ignored = true,
+        },
+        files = {
+          hidden = true,
+          ignored = true,
+          exclude = { "node_modules" },
+        },
+        grep = {
+          hidden = true,
+          ignored = true,
+          exclude = { "node_modules" },
         },
       },
     },
